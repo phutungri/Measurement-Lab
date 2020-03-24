9d4a0fddddccfe6e2a434e1d39f323da080fe9ad
rmaharjan <thebrokenendmill@users.noreply.github.com>
2020-03-24 11:17:36 +0545

    Update OCT_functions.py

--- OCT/measurements/OCT_functions.py
+++ OCT/measurements/OCT_functions.py
@@ -5,29 +5,41 @@ import matplotlib.pyplot as plt
 import utility_functions as util
 
 """
-This file includes functions required to parse, extract, analyze data acquired from Chip for OCT systems. Primary authors:
-Rijan Maharjan
-Sanket Bohora
+This file includes functions required to parse, extract, analyze data acquired from Chip for OCT systems. 
+Primary authors:
+	Rijan Maharjan
+	Sanket Bohora
 """
 
 def heterodyne_extract(experimental_series, reference_series, num_pts_sm, HP_threshold, time_step):
 	"""
-	
+	Extracts the weak signal, such as one from the chip, boosted with a local oscillator, extracted using a reference signal.
+		experimental_series – the main measured signal, like the RF signal from the balance detector.
+		reference_series – local-oscillator signal. RF signal without when chip is disconnected
+		num_pts_sm – how many data points to smooth over
+		HP_threshold – frequency above which all signals are zero'd. High Pass filter threshold
+		time_step – time difference between two consecutive data points
 	"""
 	# normalize the data with maximum of reference series
 	experimental_series = experimental_series/np.max(reference_series)
 	reference_series = reference_series/np.max(reference_series)
-	# average the data
+	# average the data using the sliding average function from utility functions
 	if num_pts_sm != 0:
 		experimental_series = util.sliding_average(experimental_series, num_pts_sm)
 		reference_series = util.sliding_average(reference_series, num_pts_sm)
+	# extract just the intereference envelope
 	interference_component_only = experimental_series - reference_series
+	# apply High Pass filter
 	HP_filtered_signal = util.HP_filter(interference_component_only, time_step, HP_threshold)
+	# extract the analytic signal, and finally the envelope
 	analytic_signal = sig.hilbert(HP_filtered_signal)
 	extracted_signal = np.abs(analytic_signal)
 	return extracted_signal
 
 def import_data_DSO_parsed(fname, num_channels):
+	"""
+	this function imports the data from an already parsed signal, from ATS parser (parses raw data)
+	"""
 	data = np.loadtxt(fname, skiprows=1)
 	time = data[:, 0]
 	wavelength = data[:, 1]
@@ -46,6 +58,9 @@ def import_data_DSO_parsed(fname, num_channels):
 		return
 
 def import_data_DSO_raw_samples(fname):
+	"""
+	imports directly saved Alazar Text Records files, and returns only the time, sample value, and scaled voltages from DSO software
+	"""
 	data = np.loadtxt(fname, skiprows=46, delimiter=',')
 	time = data[:, 1]
 	samples = data[:, 2]
@@ -53,6 +68,9 @@ def import_data_DSO_raw_samples(fname):
 	return time, samples, volts
 
 def import_data_DAQ(fname, num_channels):
+	"""
+	Function imports data acquired from the DAQ board using AlazarSDK code.
+	"""
 	data = np.loadtxt(fname, skiprows=1)
 	m, n = data.shape
 	if n != num_channels:
@@ -85,6 +103,10 @@ def sample_to_volts(sample_array, bits_per_sample, pm_range_mv):
 		return
 
 def time_to_wavelength_HSL20(time_array_sec):
+	"""
+	Uses the data provided by the manufacturer to get corresponding wavelength information from given time, using a fitted function.
+	Time must be in seconds.
+	"""
 	# convert time in micro seconds
 	time = time_array_sec * 1000 * 1000
 	wavelength = 1256 + 15.8138*time + 5.5467*time**2 - 1.0695*time**3 + 0.02803*time**4
